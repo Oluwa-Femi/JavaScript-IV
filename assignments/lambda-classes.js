2c66127e118ee3a4df8dfd15b60d34dbafec10df
Oluwatola Oluwafemi Blessing <53811965+Oluwa-Femi@users.noreply.github.com>
2019-08-22 15:02:10 +0100

    Class person done

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,14 @@
 // CODE here for your Lambda Classes
+//Create a class of Person
+//Create a class of Instructors and Students extensions to person
+//Create a class of Project manager which is an extension to Instructor
+class Person {
+    constructor(name, age, location) {
+        this.name = name;
+        this.age = age;
+        this.location = location;
+    }
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    }
+}
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -2,7 +2,7 @@
 
 Prototype Refactor
 
-1. Copy and paste your code or the solution from yesterday*/
+1(a). Copy and paste your code or the solution from yesterday*/
 /*function Players(name) {
   this.name = name;
 }
@@ -20,14 +20,14 @@ Strikers.prototype.fly = function () {
 
 var Aubameyang = new Players('Aubameyang');
 var Lacazette = new Strikers('Lacazette', 'CF');*/
-/*2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.*/
+/*1(b). Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.*/
 
 class Players {
     constructor(name) {
       this.name = name;
     }
     introduction() {
-      return 'Yo' + this.name + 'plays for Arsenal'
+      return 'Yo ' + this.name + ' plays for Arsenal'
     }
     celebration() {
       return 'siiiiiiii'
@@ -44,3 +44,40 @@ class Players {
   }
   var Aubameyang = new Players('Aubameyang');
   var Lacazette = new Strikers('Lacazette', 'CF');
+
+  /* 
+
+Prototype Refactor
+
+2a. Copy and paste your code or the solution from yesterday
+function Car(model_name, make) {
+    this.model = model_name;
+    this.make = make;
+  };
+  Car.prototype.carCrashed = function () {
+    if (this.crash === true) {
+      this.drive = false
+      console.log(`I crashed at x miles!`);
+    } else {
+      return this.drive = true;
+    }
+  }
+
+
+  2(b). Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.*/
+  /*Class Car {
+  constructor(model_name) {
+    this.model = model_name;
+  }
+  constructor(make) {
+    this.make = make;
+  }
+  car.crashed() {
+    if (this.crash === true) {
+        this.drive = false
+        console.log(`I crashed at x miles!`);
+      } else {
+        return this.drive = true;
+      }
+  }
+}*/
