7d7326651c75c880e3c019dcad86e9b003c4568b
Oluwatola Oluwafemi Blessing <53811965+Oluwa-Femi@users.noreply.github.com>
2019-08-22 14:18:03 +0100

    First refactor done

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -2,8 +2,45 @@
 
 Prototype Refactor
 
-1. Copy and paste your code or the solution from yesterday
+1. Copy and paste your code or the solution from yesterday*/
+/*function Players(name) {
+  this.name = name;
+}
+Players.prototype.introduction = function () {
+  return 'Yo' + this.name + 'plays for Arsenal'
+}
+function Strikers(name, position) {
+  this.position = position;
+  Players.call(this, name)
+}
+Strikers.prototype = Object.create(Players.prototype)
+Strikers.prototype.fly = function () {
+  return `${this.name} is a legend!`
+}
 
-2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
+var Aubameyang = new Players('Aubameyang');
+var Lacazette = new Strikers('Lacazette', 'CF');*/
+/*2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.*/
 
-*/
+class Players {
+    constructor(name) {
+      this.name = name;
+    }
+    introduction() {
+      return 'Yo' + this.name + 'plays for Arsenal'
+    }
+    celebration() {
+      return 'siiiiiiii'
+    }
+  }
+  class Strikers extends Players {
+    constructor(name, position) {
+      super(name);
+      this.position = position;
+    }
+    fly() {
+      return `${this.name} is a legend!`;
+    }
+  }
+  var Aubameyang = new Players('Aubameyang');
+  var Lacazette = new Strikers('Lacazette', 'CF');
