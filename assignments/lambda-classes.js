9ef8d101fd924d3871621d721e55a432fbf60a45
Oluwatola Oluwafemi Blessing <53811965+Oluwa-Femi@users.noreply.github.com>
2019-08-22 15:46:24 +0100

    Instructor-Student-completed

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,4 +11,47 @@ class Person {
     speak() {
         console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
     }
+};
+
+class Instructor extends Person {
+    constructor(name, age, location, specialty, favLanguage, catchPhrase) {
+        super(name, age, location);
+        this.name = name;
+        this.age = age;
+        this.location = location;
+        this.specialty = specialty;
+        this.favLanguage = favLanguage;
+        this.catchPhrase = catchPhrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`)
+    }
+    grade(student, subject) {
+        console.log(`${student} receives a perfect score on ${subject}`)
+    }
 }
+class Student extends Person {
+    constructor(name, age, location, previousBackground, className, favSubjects, listsSubjects, PRAssignment, sprintChallenge) {
+        super(name, age, location);
+        this.name = name;
+        this.age = age;
+        this.location = location;
+        this.previousBackground = previousBackground;
+        this.className = className;
+        this.favSubjects = favSubjects;
+        this.listsSubjects = listsSubjects;
+        this.PRAssignment = PRAssignment;
+        this.sprintChallenge = sprintChallenge;
+
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`)
+    }
+    grade(student, subject) {
+        console.log(`${student} receives a perfect score on ${subject}`)
+    }
+}
+class ProjectManager extends Instructor {
+
+}
+
